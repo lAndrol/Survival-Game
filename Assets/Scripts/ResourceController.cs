a2a9c0720206f992780916df42e27a855b174a8f
Erman Genco Seçilen <ermangenco@hotmail.com>
2023-05-11 22:30:56 +0300

    Score board

--- Assets/Scripts/ResourceController.cs
+++ Assets/Scripts/ResourceController.cs
@@ -1,30 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ResourceController : MonoBehaviour
 {
-    float food, mechanicalParts, construction, medicine, bullets;
 
+    public GameObject ConstructionResourceCounter;
+    public GameObject FoodResourceCounter;
+    public GameObject MechanicalPartsResourceCounter;
+    public GameObject MedicineResourceCounter;
+    public GameObject BulletsResourceCounter;
+    float food, mechanicalParts, construction, medicine, bullets;
+    void Start()
+    {
+        food = 0;
+        mechanicalParts = 0;
+        construction = 0;
+        medicine = 0;
+        bullets = 0;
+        for(int i = 0; i < 5; i++)
+        {
+            UpdateScore(i);
+        }
+    }
+    void UpdateScore(int type)
+    {
+        switch (type)
+        {
+            case 0:
+                FoodResourceCounter.GetComponent<TMPro.TextMeshProUGUI>().text = food.ToString();
+                break;
+            case 1:
+                MechanicalPartsResourceCounter.GetComponent<TMPro.TextMeshProUGUI>().text = mechanicalParts.ToString();
+                break;
+            case 2:
+                ConstructionResourceCounter.GetComponent<TMPro.TextMeshProUGUI>().text = construction.ToString();
+                break;
+            case 3:
+                MedicineResourceCounter.GetComponent<TMPro.TextMeshProUGUI>().text = medicine.ToString();
+                break;
+            case 4:
+                BulletsResourceCounter.GetComponent<TMPro.TextMeshProUGUI>().text = bullets.ToString();
+                break;
+        }
+    }
     public void ChangeFood(float amount)
     {
         this.food += amount;
+        UpdateScore(0);
     }
     public void ChangeMechanicalParts(float amount)
     {
         this.mechanicalParts += amount;
+        UpdateScore(1);
     }
     public void ChangeConstruction(float amount)
     {
         this.construction += amount;
+        UpdateScore(2);
     }
     public void ChangeMedicine(float amount)
     {
         this.medicine += amount;
+        UpdateScore(3);
     }
     public void ChangeBullets(float amount)
     {
         this.bullets += amount;
+        UpdateScore(4);
     }
     public float GetFood()
     {
